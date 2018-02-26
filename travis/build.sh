a44ae62e04ea470550234512d2548c163464ff54
alt <zeroonline@yandex.ru>
2018-02-26 22:08:06 +0300

    travis build settings

--- travis/build.sh
+++ travis/build.sh
@@ -6,10 +6,10 @@ projectdir="$(dirname "$(pwd)")"
 echo projectpath is $projectdir
 
 echo "Attempting to build $project for Android"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $projectdir/unity.log -projectPath $projectdir -buildTarget Android "$projectdir/Build/android/$project.apk" -quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $projectdir/unity.log -projectPath $projectdir -buildTarget Android "$projectdir/Build/$project.apk" -quit
 
 echo 'Logs from build'
 cat $projectdir/unity.log
 
-echo 'Attempting to zip builds'
-zip -r $projectdir/Build/android.zip $projectdir/Build/android/
+#echo 'Attempting to zip builds'
+#zip -r $projectdir/Build/android.zip $projectdir/Build/android/
