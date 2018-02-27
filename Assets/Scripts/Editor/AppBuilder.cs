47d7d65799a306cac031cee2170b86a6c95ada46
alt <zeroonline@yandex.ru>
2018-02-27 19:27:28 +0300

    fixes travis

---
+++ Assets/Scripts/Editor/AppBuilder.cs
@@ -0,0 +1,35 @@
+﻿using UnityEditor;
+
+namespace Editor
+{
+    public static class AppBuilder
+    {
+        [MenuItem("Tools/Build/Android")]
+        public static void BuildAndroid()
+        {
+            BuildApp(BuildTarget.Android);
+        }
+        
+        private static void BuildApp(BuildTarget target)
+        {
+            var options = new BuildPlayerOptions();
+            options.locationPathName = "build/application.apk";
+
+            var editorBuildSettingsScenes = EditorBuildSettings.scenes;
+            var sceneCount = editorBuildSettingsScenes.Length;
+            var scenes = new string[sceneCount];
+
+            for (int i = 0; i < sceneCount; i++)
+            {
+                var editorSettingsScene = editorBuildSettingsScenes[i];
+                if (editorSettingsScene.enabled) scenes[i] = editorSettingsScene.path;
+            }
+
+            options.scenes = scenes;
+            options.target = target;
+            options.options = BuildOptions.None;
+            
+            BuildPipeline.BuildPlayer(options);
+        }
+    }
+}
--- travis/build.sh
+++ travis/build.sh
@@ -1,12 +1,13 @@
 #! /bin/sh
 
 project="uilatebinding"
-projectdir="$(dirname "$(pwd)")"
+#projectdir="$(dirname "$(pwd)")"
+projectdir="/Users/alt/Projects/Unity/uilatebinding"
 
 echo projectpath is $projectdir
 
 echo "Attempting to build $project for Android"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $projectdir/unity.log -projectPath $projectdir -buildTarget Android "$projectdir/travis_build/app.apk" -quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $projectdir/unity.log -projectPath $projectdir  -executeMethod Editor.AppBuilder.BuildAndroid -quit
 
 echo 'Logs from build'
 cat $projectdir/unity.log
