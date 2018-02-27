9023c11d2dfabc8d2649a70bdd5126b322085705
alt <zeroonline@yandex.ru>
2018-02-27 22:31:43 +0300

    fixes travis

--- travis/install.sh
+++ travis/install.sh
@@ -23,5 +23,15 @@ install() {
 # See $BASE_URL/$HASH/unity-$VERSION-$PLATFORM.ini for complete list
 # of available packages, where PLATFORM is `osx` or `win`
 
-install "MacEditorInstaller/Unity-$VERSION.pkg"
-install "MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-$VERSION.pkg"
+curl -o android-sdk.zip http://dl.google.com/android/android-sdk_r24.4.1-macosx.zip
+unzip android-sdk.zip
+export ANDROID_HOME=$PWD/android-sdk-macosx
+export PATH=$PATH:$ANDROID_HOME/tools
+export PATH=$PATH:$ANDROID_HOME/platform-tools
+export JAVA_HOME=$(/usr/libexec/java_home)
+
+echo $ANDROID_HOME
+echo $JAVA_HOME
+
+#install "MacEditorInstaller/Unity-$VERSION.pkg"
+#install "MacEditorTargetInstaller/UnitySetup-Android-Support-for-Editor-$VERSION.pkg"
