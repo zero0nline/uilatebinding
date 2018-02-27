05278e49dbda94cd6bede1a4cdd3971d3d30d73a
alt <zeroonline@yandex.ru>
2018-02-27 16:37:24 +0300

    fixes travis

--- travis/build.sh
+++ travis/build.sh
@@ -6,7 +6,7 @@ projectdir="$(dirname "$(pwd)")"
 echo projectpath is $projectdir
 
 echo "Attempting to build $project for Android"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $projectdir/unity.log -projectPath $projectdir -buildTarget Android "$projectdir/build/$project.apk" -quit
+/Applications/Unity/Unity.app/Contents/MacOS/Unity -batchmode -nographics -silent-crashes -logFile $projectdir/unity.log -projectPath $projectdir -buildTarget Android "$projectdir/travis_build/app.apk" -quit
 
 echo 'Logs from build'
 cat $projectdir/unity.log
